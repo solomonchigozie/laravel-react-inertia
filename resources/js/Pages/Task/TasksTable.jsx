69b4994ddfdb7c21fe7dc9520ae3538e9f034aee
solomon chigozie <solomon1chigozie@gmail.com>
2024-09-20 22:04:55 +0100

    update

--- resources/js/Pages/Task/TasksTable.jsx
+++ resources/js/Pages/Task/TasksTable.jsx
@@ -145,8 +145,14 @@ export default function TasksTable({ tasks, queryParams = null, hideProjectColum
                             <tr className='' key={task.id}>
                                 <td className='px-3 py-2 p-4 border-b border-blue-gray-100 bg-blue-gray-50'>{task.id}</td>
                                 <td className='px-3 py-2 p-4 border-b border-blue-gray-100 bg-blue-gray-50'> <img src={task.image_path} alt={task.name} style={{ width: 60 }} /> </td>
-                                {!hideProjectColumn && <td className='px-3 py-2 p-4 border-b border-blue-gray-100 bg-blue-gray-50'>{task.project.name}</td>}
-                                <td className='px-3 py-2 p-4 border-b border-blue-gray-100 bg-blue-gray-50'>{task.name}</td>
+                                {!hideProjectColumn && 
+                                <td className='px-3 py-2 p-4 border-b border-blue-gray-100 bg-blue-gray-50'>{task.project.name}</td>
+                                }
+                                <td className='px-3 py-2 p-4 border-b border-blue-gray-100 bg-blue-gray-50 hover:underline'>
+                                    <Link href={route('task.show',task.id)}>
+                                        {task.name}
+                                    </Link> 
+                                </td>
                                 <td className='px-3 py-2 p-4 border-b border-blue-gray-100 bg-blue-gray-50'>
                                     <span className={'px-2 py-1 rounded text-white ' +
                                         TASK_STATUS_CLASS_MAP[task.status]
